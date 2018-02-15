e352b423b15d63da61782c07487f581b4668db6d
Joao Paulo Magalhaes <dev@jpmag.me>
2018-02-15 12:57:05 +0000

    fix error in c4_add_target

--- c4project.cmake
+++ c4project.cmake
@@ -53,11 +53,14 @@ function(c4_add_target prefix name)
         if(_c4al_FOLDER)
             set_target_properties(${name} PROPERTIES FOLDER "${_c4al_FOLDER}")
         endif()
-        set_target_properties(${name} PROPERTIES COMPILE_FLAGS ${${uprefix}CXX_FLAGS})
+        if(${uprefix}CXX_FLAGS)
+            set_target_properties(${name} PROPERTIES COMPILE_FLAGS ${${uprefix}CXX_FLAGS})
+        endif()
+        if(${uprefix}LINT)
+            static_analysis_target(${ucprefix} ${name} "${_c4al_FOLDER}" lint_targets)
+        endif()
     endif()
-    if(${uprefix}LINT)
-        static_analysis_target(${ucprefix} ${name} "${_c4al_FOLDER}" lint_targets)
-    endif()
+
     if(_c4al_SANITIZE AND ${uprefix}SANITIZE)
         sanitize_target(${name} ${lcprefix}
             ${_what}
