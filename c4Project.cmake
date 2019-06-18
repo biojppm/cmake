a97b507eb8f2faaa2a546672d254519bc15dcf61
Joao Paulo Magalhaes <dev@jpmag.me>
2019-06-18 12:57:59 +0200

    git version WIP

--- c4Project.cmake
+++ c4Project.cmake
@@ -229,6 +229,37 @@ function(c4_declare_project prefix)
 endfunction(c4_declare_project)
 
 
+function(c4_proj_version dir)
+
+    if("${dir}" STREQUAL "")
+        set(dir ${CMAKE_CURRENT_LIST_DIR})
+    endif()
+
+    # http://xit0.org/2013/04/cmake-use-git-branch-and-commit-details-in-project/
+
+    # Get the current working branch
+    execute_process(
+        COMMAND git rev-parse --abbrev-ref HEAD
+        WORKING_DIRECTORY ${dir}
+        OUTPUT_VARIABLE GIT_BRANCH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+    # Get the latest abbreviated commit hash of the working branch
+    execute_process(
+        COMMAND git log -1 --format=%h
+        WORKING_DIRECTORY ${dir}
+        OUTPUT_VARIABLE GIT_COMMIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+    # also: git diff --stat
+    # also: git diff
+    # also: git status --ignored
+
+endfunction()
+
+
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
