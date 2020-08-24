d21f02cb5e14afa2754e54b108fd70144d4faf83
Joao Paulo Magalhaes <dev@jpmag.me>
2020-08-25 00:06:15 +0100

    some of the pedantic flags are not available in earlier GCC versions

--- c4Project.cmake
+++ c4Project.cmake
@@ -354,63 +354,6 @@ function(c4_declare_project prefix)
     c4_dbg_var_if(${_c4_uprefix}CXX_FLAGS_OPT)
 endfunction(c4_declare_project)
 
-# default pedantic flags taken from:
-# https://github.com/lefticus/cpp_starter_project/blob/master/cmake/CompilerWarnings.cmake
-set(_C4_PEDANTIC_FLAGS_MSVC
-    /W4 # Baseline reasonable warnings
-    /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
-    /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
-    /w14263 # 'function': member function does not override any base class virtual member function
-    /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
-            # be destructed correctly
-    /w14287 # 'operator': unsigned/negative constant mismatch
-    /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
-            # the for-loop scope
-    /w14296 # 'operator': expression is always 'boolean_value'
-    /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
-    /w14545 # expression before comma evaluates to a function which is missing an argument list
-    /w14546 # function call before comma missing argument list
-    /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
-    /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
-    /w14555 # expression has no effect; expected expression with side- effect
-    /w14619 # pragma warning: there is no warning number 'number'
-    /w14640 # Enable warning on thread un-safe static member initialization
-    /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
-    /w14905 # wide string literal cast to 'LPSTR'
-    /w14906 # string literal cast to 'LPWSTR'
-    /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
-    /permissive- # standards conformance mode for MSVC compiler.
-    )
-
-set(_C4_PEDANTIC_FLAGS_CLANG
-    -Wall
-    -Wextra
-    -pedantic
-    -Wshadow # warn the user if a variable declaration shadows one from a parent context
-    -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
-                       # catch hard to track down memory errors
-    #-Wold-style-cast # warn for c-style casts
-    -Wcast-align # warn for potential performance problem casts
-    -Wunused # warn on anything being unused
-    -Woverloaded-virtual # warn if you overload (not override) a virtual function
-    -Wpedantic # warn if non-standard C++ is used
-    -Wconversion # warn on type conversions that may lose data
-    -Wsign-conversion # warn on sign conversions
-    -Wnull-dereference # warn if a null dereference is detected
-    -Wdouble-promotion # warn if float is implicit promoted to double
-    -Wfloat-equal # warn if comparing floats
-    -Wformat=2 # warn on security issues around functions that format output (ie printf)
-    )
-
-set(_C4_PEDANTIC_FLAGS_GCC
-    ${_C4_PEDANTIC_FLAGS_CLANG}
-    -Wmisleading-indentation # where indentation implies blocks where blocks do not exist
-    -Wduplicated-cond # where if-else chain has duplicated conditions
-    -Wduplicated-branches # where if-else branches have duplicated code
-    -Wlogical-op # where logical operations are used where bitwise were probably wanted
-    -Wuseless-cast # where you perform a cast to the same type
-    )
-
 
 function(c4_add_dev_targets)
     if(NOT CMAKE_CURRENT_LIST_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
@@ -485,6 +428,73 @@ macro(c4_optional_compile_flags_dev tag desc)
         c4_setg(${_c4_uprefix}CXX_FLAGS_OPT "${${_c4_uprefix}CXX_FLAGS_OPT};${flags}")
     endif()
 endmacro()
+
+# pedantic flags...
+# default pedantic flags taken from:
+# https://github.com/lefticus/cpp_starter_project/blob/master/cmake/CompilerWarnings.cmake
+set(_C4_PEDANTIC_FLAGS_MSVC
+    /W4 # Baseline reasonable warnings
+    /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
+    /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+    /w14263 # 'function': member function does not override any base class virtual member function
+    /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
+            # be destructed correctly
+    /w14287 # 'operator': unsigned/negative constant mismatch
+    /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
+            # the for-loop scope
+    /w14296 # 'operator': expression is always 'boolean_value'
+    /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
+    /w14545 # expression before comma evaluates to a function which is missing an argument list
+    /w14546 # function call before comma missing argument list
+    /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
+    /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
+    /w14555 # expression has no effect; expected expression with side- effect
+    /w14619 # pragma warning: there is no warning number 'number'
+    /w14640 # Enable warning on thread un-safe static member initialization
+    /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
+    /w14905 # wide string literal cast to 'LPSTR'
+    /w14906 # string literal cast to 'LPWSTR'
+    /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+    /permissive- # standards conformance mode for MSVC compiler.
+    )
+
+set(_C4_PEDANTIC_FLAGS_CLANG
+    -Wall
+    -Wextra
+    -pedantic
+    -Wshadow # warn the user if a variable declaration shadows one from a parent context
+    -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+                       # catch hard to track down memory errors
+    #-Wold-style-cast # warn for c-style casts
+    -Wcast-align # warn for potential performance problem casts
+    -Wunused # warn on anything being unused
+    -Woverloaded-virtual # warn if you overload (not override) a virtual function
+    -Wpedantic # warn if non-standard C++ is used
+    -Wconversion # warn on type conversions that may lose data
+    -Wsign-conversion # warn on sign conversions
+    -Wdouble-promotion # warn if float is implicit promoted to double
+    -Wfloat-equal # warn if comparing floats
+    -Wformat=2 # warn on security issues around functions that format output (ie printf)
+    )
+
+set(_C4_PEDANTIC_FLAGS_GCC ${_C4_PEDANTIC_FLAGS_CLANG}
+    -Wlogical-op # where logical operations are used where bitwise were probably wanted
+    -Wuseless-cast # where you perform a cast to the same type
+    )
+
+if((CMAKE_CXX_COMPILER_ID STREQUAL "GNU") AND (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 6.0))
+    list(APPEND _C4_PEDANTIC_FLAGS_GCC
+        -Wnull-dereference # warn if a null dereference is detected
+        -Wmisleading-indentation # where indentation implies blocks where blocks do not exist
+        -Wduplicated-cond # where if-else chain has duplicated conditions
+        )
+endif()
+
+if((CMAKE_CXX_COMPILER_ID STREQUAL "GNU") AND (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 7.0))
+    list(APPEND _C4_PEDANTIC_FLAGS_GCC
+        -Wduplicated-branches # where if-else branches have duplicated code
+        )
+endif()
 
 
 #------------------------------------------------------------------------------
