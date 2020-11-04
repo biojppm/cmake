3c8b346cfb7cc08ecf1ba9a79168b61945cf2632
Joao Paulo Magalhaes <dev@jpmag.me>
2020-11-04 14:16:47 +0000

    new: usr: add arm toolchain

---
+++ Toolchain-Armv7.cmake
@@ -0,0 +1,68 @@
+## Target system
+#SET(CMAKE_SYSTEM_NAME Linux)
+#SET(CMAKE_SYSTEM_PROCESSOR arm)
+#SET(CMAKE_SYSTEM_VERSION 1)
+#set(CMAKE_CROSSCOMPILING TRUE)
+#
+#find_program(CC_GCC arm-none-linux-gnueabihf-gcc REQUIRED)
+#
+#set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
+#
+## Cross compiler
+#SET(CMAKE_C_COMPILER   arm-none-linux-gnueabihf-gcc)
+#SET(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+#set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+#
+## Search for programs in the build host directories
+#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#
+## Libraries and headers in the target directories
+#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+#
+#set(THREADS_PTHREAD_ARG "0" CACHE STRING "Result from TRY_RUN" FORCE)
+#
+#
+#return()
+
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_CROSSCOMPILING 1)
+
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
+
+
+set(CMAKE_FIND_ROOT_PATH /usr/arm-none-eabi)
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(COMPILER_FLAGS "-marm -mfpu=neon  -mfloat-abi=hard -mcpu=cortex-a9 -D_GNU_SOURCE")
+
+message(STATUS)
+message(STATUS)
+message(STATUS)
+
+if(NOT DEFINED CMAKE_C_FLAGS)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}" CACHE STRING "")
+else()
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}")
+endif()
+
+message(STATUS)
+message(STATUS)
+message(STATUS)
+message(STATUS)
+
+if(NOT DEFINED CMAKE_CXX_FLAGS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILER_FLAGS}" CACHE STRING "")
+else()
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILER_FLAGS}")
+endif()
