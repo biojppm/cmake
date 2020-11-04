843aed915d48835bfa561706fdc6a0ae259ff348
Joao Paulo Magalhaes <dev@jpmag.me>
2020-11-04 16:18:16 +0000

    new: usr: run tests in cross-compile

--- Toolchain-Armv7.cmake
+++ Toolchain-Armv7.cmake
@@ -1,4 +1,14 @@
-# Target system
+# taken from https://stackoverflow.com/a/49086560
+
+# tested with the toolchain from ARM:
+# gcc-arm-9.2-2019.12-mingw-w64-i686-arm-none-linux-gnueabihf.tar.xz
+# found at
+# https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-a/downloads
+
+# see also:
+# https://stackoverflow.com/questions/42371788/how-to-run-helloworld-on-arm
+# https://dev.to/younup/cmake-on-stm32-the-beginning-3766
+
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_SYSTEM_VERSION 1)
@@ -11,7 +21,7 @@ set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
 # Cross compiler
 SET(CMAKE_C_COMPILER   arm-none-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
-set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+set(CMAKE_LIBRARY_ARCHITECTURE arm-none-linux-gnueabihf)
 
 # Search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -23,6 +33,12 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(THREADS_PTHREAD_ARG "0" CACHE STRING "Result from TRY_RUN" FORCE)
 
+get_filename_component(TOOLCHAIN_DIR "${CC_GCC}" DIRECTORY)
+get_filename_component(TOOLCHAIN_DIR "${TOOLCHAIN_DIR}" DIRECTORY)
+set(TOOLCHAIN_SO_DIR "${TOOLCHAIN_DIR}/arm-none-linux-gnueabihf/libc/")
+
+#/home/jpmag/local/arm/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf
+set(CMAKE_CROSSCOMPILING_EMULATOR qemu-arm -L ${TOOLCHAIN_SO_DIR})
 
 return()
 
