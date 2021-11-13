84696d52e838097c65f08dab5576082f0545e0d5
Joao Paulo Magalhaes <dev@jpmag.me>
2021-11-13 12:01:51 +0000

    [impl] add amalgamate utils

--- amalgamate_utils.py
+++ amalgamate_utils.py
@@ -1,3 +1,4 @@
+import re
 
 
 class cmtfile:
@@ -21,7 +22,26 @@ class ignfile:
     def __init__(self, filename):
         self.filename = filename
     def __str__(self):
-        return self.text
+        return self.filename
+
+
+class hdrfile:
+    """header file, with custom include guard"""
+    def __init__(self, filename, incpattern, include_guard=None):
+        self.filename = filename
+        self.incpattern = incpattern
+        self.include_guard = include_guard
+    def __str__(self):
+        return self.filename
+
+
+class injfile:
+    """header file, to be injected at the first include point"""
+    def __init__(self, filename, incpattern):
+        self.filename = filename
+        self.incpattern = incpattern
+    def __str__(self):
+        return self.filename
 
 
 def catfiles(filenames, rootdir,
@@ -31,30 +51,40 @@ def catfiles(filenames, rootdir,
              result_incguard):
     sepb = "//" + ("**" * 40)
     sepf = "//" + ("--" * 40)
+    to_inject = {}
+    custom_include_guards = {}
     def banner(s):
         return f"\n\n\n{sepb}\n{sepf}\n// {s}\n// {repo}/{s}\n{sepf}\n{sepb}\n\n"
     def footer(s):
         return f"\n\n// (end {repo}/{s})\n"
     def incguard(filename):
-        return f"{filename.replace('.','_').replace('/','_').upper()}_"
-    def replace_include(rx, match, line):
+        return custom_include_guards.get(filename,
+                                         f"{filename.replace('.','_').replace('/','_').upper()}_")
+    def replace_include(rx, match, line, guard):
         line = line.rstrip()
-        cline = rx.sub(f"//{line}", line)
         incl = match.group(1)
-        guard = incguard(incl)
-        return f"""{cline}
-#if !defined({guard}) && !defined(_{guard}) /* {repo}/src/{incl} */
+        if to_inject.get(incl) is None:
+            if guard is None:
+                guard = incguard(incl)
+            return f"""// amalgamate: removed include of
+// {repo}/src/{incl}
+//{line}
+#if !defined({guard}) && !defined(_{guard})
 #error "amalgamate: file {incl} must have been included at this point"
-#endif /* {guard} - {repo}/src/{incl} */\n
+#endif /* {guard} */\n
 """
-    def append_file(filename):
+        else:
+            entry = to_inject[incl]
+            del to_inject[incl]
+            return append_file(entry.filename)
+    def append_file(filename, guard=None):
         s = ""
         with open(filename) as f:
             for line in f.readlines():
                 for rx in include_regexes:
                     match = rx.match(line)
                     if match:
-                        line = replace_include(rx, match, line)
+                        line = replace_include(rx, match, line, guard)
                 s += line
         return s
     def append_cpp(filename):
@@ -66,7 +96,9 @@ def catfiles(filenames, rootdir,
         return filename.endswith(".cpp") or filename.endswith(".c")
     out = ""
     for entry in filenames:
-        if isinstance(entry, cmttext):
+        if isinstance(entry, ignfile):
+            pass
+        elif isinstance(entry, cmttext):
             for line in entry.text.split("\n"):
                 out += f"// {line}\n"
         elif isinstance(entry, cmtfile):
@@ -75,17 +107,51 @@ def catfiles(filenames, rootdir,
             with open(filename) as file:
                 for line in file.readlines():
                     out += f"// {line}"
-        elif isinstance(entry, ignfile):
-            pass
+        elif isinstance(entry, injfile):
+            entry.filename = f"{rootdir}/{entry.filename}"
+            to_inject[entry.incpattern] = entry
         else:
-            assert isinstance(entry, str)
             filename = f"{rootdir}/{entry}"
-            append = append_cpp if is_src(filename) else append_file
             out += banner(entry)
-            out += append(filename)
+            if isinstance(entry, hdrfile):
+                if entry.include_guard is not None:
+                    custom_include_guards[entry.incpattern] = entry.include_guard
+                out += append_file(filename, entry.include_guard)
+            else:
+                assert isinstance(entry, str)
+                if is_src(filename):
+                    out += append_cpp(filename)
+                else:
+                    out += append_file(filename)
             out += footer(entry)
-    out = f"""#ifndef {result_incguard}
+    return f"""#ifndef {result_incguard}
 {out}
 #endif /* {result_incguard} */
 """
+
+def include_only_first(file_contents: str):
+    rx = [
+        re.compile(r'^\s*#\s*include "(.*?)".*'),
+        re.compile(r'^\s*#\s*include <(.*?)>.*'),
+    ]
+    already_included = {}
+    out = ""
+    for line in file_contents.split("\n"):
+        for expr in rx:
+            match = expr.match(line)
+            if match:
+                incl = match.group(1)
+                if already_included.get(incl) is None:
+                    already_included[incl] = line
+                    if incl.endswith(".h"):
+                        cpp_version = f"c{incl[:-2]}"
+                        already_included[cpp_version] = line
+                    elif incl.startswith("c") and not (incl.endswith(".h") or incl.endswith(".hpp")):
+                        c_version = f"{incl[1:]}.h"
+                        already_included[c_version] = line
+                else:
+                    line = f"//included above:\n//{line}"
+                break
+        out += line
+        out += "\n"
     return out
